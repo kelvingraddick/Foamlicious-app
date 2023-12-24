cd303fe01f8dae9e7a48f6e2b939a49a46e1b24e
Kelvin <kelvingraddick@kg.codes>
2023-12-23 19:25:00 -0500

    Boot screen implementation/fixes for Android

--- android/app/src/main/java/com/foamlicious/MainActivity.java
+++ android/app/src/main/java/com/foamlicious/MainActivity.java
@@ -1,11 +1,19 @@
 package com.wavelinkllc.foamlicious;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this, R.style.SplashTheme); // ⬅️ initialize the splash screen
+    super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
